8192e45a136c0dd39df3083de268bde0362928bc
RichardCasey <RichardCasey@VN0663>
2024-10-16 13:52:21 +0100

    Add project files.

---
+++ Script.js
@@ -0,0 +1,7 @@
+﻿document.querySelectorAll('details').forEach((detail) => {
+    detail.addEventListener('toggle', (event) => {
+        if (event.target.open) {
+            console.log(event.target.querySelector('summary').textContent + ' opened');
+        }
+    });
+});
